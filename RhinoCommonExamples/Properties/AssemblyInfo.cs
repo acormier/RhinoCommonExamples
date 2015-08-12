90fd96746973bc1769e828ea705bfaca2d8ba3e0
alain <alain@mcneel.com>
2015-08-12 15:32:33 +0200

    initial commit

---
+++ RhinoCommonExamples/Properties/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+using Rhino.PlugIns;
+
+// Plug-in Description Attributes - all of these are optional
+// These will show in Rhino's option dialog, in the tab Plug-ins
+[assembly: PlugInDescription (DescriptionType.Address, "")]
+[assembly: PlugInDescription (DescriptionType.Country, "")]
+[assembly: PlugInDescription (DescriptionType.Email, "")]
+[assembly: PlugInDescription (DescriptionType.Phone, "")]
+[assembly: PlugInDescription (DescriptionType.Organization, "")]
+[assembly: PlugInDescription (DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription (DescriptionType.WebSite, "")]
+
+// Information about this assembly is defined by the following attributes.
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle ("RhinoCommonExamples")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion ("1.0.*")]
+
+// Rhino requires a Guid assigned to the assembly. Xamarin Studio can't insert a Guid in file templates automatically.
+[assembly: Guid ("ae8a4e80-13ca-4fe8-b49a-a835f9b9aba5")]
